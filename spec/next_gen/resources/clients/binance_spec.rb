0981d8775abdd51b524f98b5ff214fc8d9558bdb
Stefano Baldazzi <stefano.baldazzi@zero.it>
2025-02-14 16:24:48 +0100

    R360-03 update binance client response with unix_timestamp & date format

--- spec/next_gen/resources/clients/binance_spec.rb
+++ spec/next_gen/resources/clients/binance_spec.rb
@@ -15,7 +15,8 @@ RSpec.describe NextGen::Clients::Binance do
         expect(response).not_to be_empty
 
         first_candle = response.first
-        expect(first_candle).to eq({ time: 1_699_102_800_000, price: 34_758.78000000 })
+        expected_date = Time.at(1_699_102_800_000 / 1000)
+        expect(first_candle).to eq({ unix_timestamp: 1_699_102_800_000, price: 34_758.78000000, date: expected_date })
       end
     end
 
@@ -28,7 +29,8 @@ RSpec.describe NextGen::Clients::Binance do
         expect(response).not_to be_empty
 
         first_candle = response.first
-        expect(first_candle).to eq({ time: 1_699_102_800_000, price: 34_758.78000000 })
+        expected_date = Time.at(1_699_102_800_000 / 1000)
+        expect(first_candle).to eq({ unix_timestamp: 1_699_102_800_000, price: 34_758.78000000, date: expected_date })
       end
     end
   end
