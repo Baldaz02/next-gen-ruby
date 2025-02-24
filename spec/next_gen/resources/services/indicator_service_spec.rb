11a8a12a0ff339027a8d3f0bd425343325a5c255
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-02-24 18:27:23 +0100

    R360-03 add indicator service for simple moving averages

---
+++ spec/next_gen/resources/services/indicator_service_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+RSpec.describe NextGen::Services::IndicatorService do
+  let(:csv_file) { 'spec/fixtures/data/tickers.csv' }
+
+  let(:tickers) do
+    CSV.read(csv_file, headers: true).map do |entry|
+      NextGen::Models::Ticker.new(
+        entry['Date'],
+        entry['Open Price'].to_f,
+        entry['Close Price'].to_f,
+        entry['High Price'].to_f,
+        entry['Low Price'].to_f,
+        entry['Volume'].to_f,
+        entry['Crypto Name']
+      )
+    end
+  end
+
+  context 'sma' do
+    it do
+      sma = described_class.new(tickers).simple_moving_averages
+      date_time = Time.at(1740384000)
+
+      first_sma = sma['sma10']
+      expect(Time.parse(first_sma.date_time)).to eq date_time
+      expect(first_sma.sma).to eq 95800.08700000001
+
+      second_sma = sma['sma20']
+      expect(Time.parse(second_sma.date_time)).to eq date_time
+      expect(second_sma.sma).to eq 95745.1125
+    end
+  end
+end
