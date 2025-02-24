29f57997a7bfa3ac783be6f00a90273d709cc78f
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-02-24 15:28:08 +0100

    R360-03 add trend following groupindicators

---
+++ lib/next_gen/resources/models/indicator.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'technical-analysis'
+
+module NextGen
+  module Models
+    class Indicator
+      def self.calculate(type, period, cache_data, options: nil)
+        indicator_class = Object.const_get("TechnicalAnalysis::#{type}")
+
+        if options
+          indicator_class.calculate(cache_data[period], options)
+        else
+          indicator_class.calculate(cache_data[period], period: period, price_key: :close)
+        end
+      end
+    end
+  end
+end
