6d77cece760bab63ca10600e31be2f50897d2422
Stefano Baldazzi <stefano.baldazzi@zero.it>
2025-02-26 16:01:37 +0100

    R360-03 fix rubocop

--- lib/next_gen/resources/models/indicator.rb
+++ lib/next_gen/resources/models/indicator.rb
@@ -8,14 +8,12 @@ module NextGen
       def self.calculate(type, period, cache_data, options: nil)
         indicator_class = Object.const_get("TechnicalAnalysis::#{type}")
 
-        if type == :Obv || type == :Vwap || type == :Adi
+        if %i[Obv Vwap Adi].include?(type)
           indicator_class.calculate(cache_data[period])
+        elsif options
+          indicator_class.calculate(cache_data[period], options)
         else
-          if options
-            indicator_class.calculate(cache_data[period], options)
-          else
-            indicator_class.calculate(cache_data[period], period: period, price_key: :close)
-          end
+          indicator_class.calculate(cache_data[period], period: period, price_key: :close)
         end
       end
     end
