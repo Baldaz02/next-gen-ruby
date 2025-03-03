d147a9eaa7c25032fc1555b0263a3dcf85588cc3
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-03 15:54:38 +0100

    R360-03 init to refactor code

--- lib/next_gen/resources/services/indicator_service.rb
+++ lib/next_gen/resources/services/indicator_service.rb
@@ -3,7 +3,7 @@
 module NextGen
   module Services
     class IndicatorService
-      attr_reader :price_data, :cache_data
+      attr_reader :indicator_obj
 
       INDICATOR_CLASSES = {
         trend_following: Indicators::TrendFollowing,
@@ -13,10 +13,9 @@ module NextGen
         sentiment: Indicators::Sentiment
       }.freeze
 
-      def initialize(tickers, buffer_size = 5)
-        @price_data = load_data(tickers).freeze
-        @cache_data = {}
-        cache_data_by_periods([10, 14, 20, 34, 38], buffer_size)
+      def initialize(tickers)
+        price_data = load_data(tickers).freeze
+        @indicator_obj = Models::Indicator.new(price_data)
 
         initialize_indicators
       end
@@ -31,7 +30,7 @@ module NextGen
 
       def initialize_indicators
         INDICATOR_CLASSES.each do |indicator, klass|
-          instance_variable_set("@#{indicator}", klass.new(@cache_data))
+          instance_variable_set("@#{indicator}", klass.new(indicator_obj))
         end
       end
 
@@ -40,10 +39,6 @@ module NextGen
           { date_time: t.date, open: t.open_price, high: t.high_price,
             low: t.low_price, close: t.close_price, volume: t.volume }
         end
-      end
-
-      def cache_data_by_periods(periods, buffer_size)
-        periods.each { |period| @cache_data[period] ||= price_data.last(period + buffer_size) }
       end
     end
   end
