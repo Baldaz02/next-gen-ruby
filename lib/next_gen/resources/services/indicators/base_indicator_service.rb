f70f4e3db51ce05b9d5bf307c7f425c06409443e
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-04 20:10:13 +0100

    R360-03 refactor code

---
+++ lib/next_gen/resources/services/indicators/base_indicator_service.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module NextGen
+  module Services
+    module Indicators
+      class BaseIndicatorService
+        attr_reader :indicator_obj
+
+        def initialize(indicator_obj)
+          @indicator_obj = indicator_obj
+        end
+
+        def calculate_all
+          results = self.class::OPTIONS.each_with_object({}) do |(type, opts), result|
+            result[:"#{type}_values"] = calculate_indicator_for(type, opts)
+          end
+
+          OpenStruct.new(results)
+        end
+
+        private
+
+        def calculate_indicator_for(type, opts)
+          if %i[sma ema].include?(type)
+            calculate_moving_average(type)
+          else
+            calculate_indicator(type, opts)
+          end
+        end
+
+        def calculate_moving_average(type)
+          OpenStruct.new(
+            self.class::OPTIONS[type].transform_values { |opts| calculate_indicator(type, opts) }
+          )
+        end
+
+        def calculate_indicator(type, opts = nil)
+          indicator_obj.calculate(type.to_s, opts || self.class::OPTIONS[type])
+        end
+      end
+    end
+  end
+end
--- lib/next_gen/resources/services/indicators/momentum.rb
+++ lib/next_gen/resources/services/indicators/momentum.rb
@@ -3,30 +3,12 @@
 module NextGen
   module Services
     module Indicators
-      class Momentum
-        attr_reader :indicator_obj
-
+      class Momentum < BaseIndicatorService
         OPTIONS = {
           rsi: { period: 14, price_key: :close },
           sr: {},
           tsi: { low_period: 13, high_period: 25, price_key: :close }
         }.freeze
-
-        def initialize(indicator_obj)
-          @indicator_obj = indicator_obj
-        end
-
-        def calculate_all
-          OpenStruct.new(OPTIONS.transform_keys { |type| :"#{type}_values" }
-                               .transform_values { |opts| calculate_indicator(opts) })
-        end
-
-        private
-
-        def calculate_indicator(opts)
-          type = opts.empty? ? :sr : OPTIONS.key(opts)
-          indicator_obj.calculate(type.to_s, opts)
-        end
       end
     end
   end
