a4276824de4896ec06e9110698982fcdfa0f7d54
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-04 12:10:13 +0100

    R360-03 refactor code

--- lib/next_gen/resources/services/indicators/volatility.rb
+++ lib/next_gen/resources/services/indicators/volatility.rb
@@ -3,28 +3,12 @@
 module NextGen
   module Services
     module Indicators
-      class Volatility
-        attr_reader :indicator_obj
-
+      class Volatility < BaseIndicatorService
         OPTIONS = {
           bb: { period: 20, price_key: :close },
           atr: {},
           kc: {}
         }.freeze
-
-        def initialize(indicator_obj)
-          @indicator_obj = indicator_obj
-        end
-
-        def calculate_all
-          OpenStruct.new(OPTIONS.to_h { |type, opts| [:"#{type}_values", calculate_indicator(type, opts)] })
-        end
-
-        private
-
-        def calculate_indicator(type, opts)
-          indicator_obj.calculate(type.to_s, opts)
-        end
       end
     end
   end
