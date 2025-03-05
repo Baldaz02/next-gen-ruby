086198b9fdbd64c11fc56e3fa359e0e098e70b2b
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-05 16:00:55 +0100

    R360-04 refactor code

--- lib/next_gen/resources/services/crypto_report_service.rb
+++ lib/next_gen/resources/services/crypto_report_service.rb
@@ -6,42 +6,21 @@ require 'ostruct'
 module NextGen
   module Services
     class CryptoReportService
-      DEFAULT_PARAMS = { interval: '1h', limit: 50 }.freeze
-      FILE_PATH = 'data/cryptos.csv'
-
       attr_reader :cryptos
 
       def initialize
-        @cryptos = load_cryptos
+        @cryptos = Models::Crypto.all
       end
 
       def call
         json = []
         cryptos.each do |crypto|
-          tickers = fetch_ticker_data(crypto)
+          tickers = crypto.tickers
           indicators = Services::IndicatorService.new(tickers).calculate_all
           json << Models::CryptoMarketData.new(crypto, tickers, indicators).to_json
         end
 
         json
-      end
-
-      private
-
-      def fetch_ticker_data(crypto)
-        params = DEFAULT_PARAMS.merge(symbol: "#{crypto.symbol}USDT")
-        client = Clients::Binance.new(OpenStruct.new(params))
-        parse_data(client.candlestick, crypto)
-      end
-
-      def load_cryptos
-        CSV.foreach(FILE_PATH, headers: true).map do |row|
-          Models::Crypto.new(name: row['Name'], symbol: row['Symbol'])
-        end
-      end
-
-      def parse_data(data, crypto)
-        data.map { |entry| Models::Ticker.new(entry, crypto) }
       end
     end
   end
