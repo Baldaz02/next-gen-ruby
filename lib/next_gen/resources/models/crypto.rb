31c6ae3193a96923626178bce6f11fbba6cc9aa5
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-05 16:00:55 +0100

    R360-04 refactor code

--- lib/next_gen/resources/models/crypto.rb
+++ lib/next_gen/resources/models/crypto.rb
@@ -3,10 +3,26 @@
 module NextGen
   module Models
     class Crypto
+      DEFAULT_TICKER_PARAMS = { interval: '1h', limit: 50 }.freeze
+      FILE_PATH = 'data/cryptos.csv'
+
       attr_reader :name, :symbol
 
       def initialize(params)
         @name, @symbol = params.values_at(:name, :symbol)
+      end
+
+      def self.all
+        CSV.foreach(FILE_PATH, headers: true).map do |row|
+          Models::Crypto.new(name: row['Name'], symbol: row['Symbol'])
+        end
+      end
+
+      def tickers(params = nil)
+        params = params || DEFAULT_TICKER_PARAMS.merge(symbol: "#{symbol}USDT")
+        client = Clients::Binance.new(OpenStruct.new(params))
+
+        client.candlestick.map { |entry| Models::Ticker.new(entry, self) }
       end
     end
   end
