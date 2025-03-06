d73dc5014f9daf14ec0c4ef33363ccf980de4432
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-06 18:55:19 +0100

    R360-04 init refactor code

--- lib/next_gen/resources/models/ticker.rb
+++ lib/next_gen/resources/models/ticker.rb
@@ -11,6 +11,21 @@ module NextGen
 
         @crypto = crypto
       end
+
+      def self.sorted_by_date(tickers)
+        tickers.sort_by { |ticker| -ticker.date.to_time.to_i }
+      end
+
+      def to_h
+        {
+          date: date,
+          open_price: open_price,
+          close_price: close_price,
+          high_price: high_price,
+          low_price: low_price,
+          volume: volume
+        }
+      end
     end
   end
 end
