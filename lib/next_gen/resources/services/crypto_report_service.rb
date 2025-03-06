8120c65a6f375d228ffa0fbb15d8bbbcd73b1d79
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-06 18:05:29 +0100

    R360-04 export json with tickers & indicators

--- lib/next_gen/resources/services/crypto_report_service.rb
+++ lib/next_gen/resources/services/crypto_report_service.rb
@@ -13,14 +13,32 @@ module NextGen
       end
 
       def call
-        json = []
         cryptos.each do |crypto|
           tickers = crypto.tickers
           indicators = Services::IndicatorService.new(tickers).calculate_all
-          json << Models::CryptoMarketData.new(crypto, tickers, indicators).to_json
-        end
 
-        json
+          file_path = data_file_path(crypto)
+          data = Models::CryptoMarketData.new(crypto, tickers, indicators).to_h
+
+          save_data(file_path, data)
+        end
+      end
+
+      private
+
+      def data_file_path(crypto)
+        base_path = File.join(Dir.pwd, 'data', Time.now.strftime('%Y-%m-%d %H'))
+        file_path = File.join(base_path, "#{crypto.name}.json")
+
+        file_path.sub!('data', 'spec/data') if defined?(RSpec)
+
+        FileUtils.mkdir_p(File.dirname(file_path))
+
+        file_path
+      end
+
+      def save_data(file_path, data)
+        File.write(file_path, JSON.pretty_generate(data))
       end
     end
   end
