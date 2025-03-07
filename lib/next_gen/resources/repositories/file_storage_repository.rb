5cf45304797e8b1ee4f4bc289a5d8d17fa573477
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-07 14:17:35 +0100

    R360-04 refactor code

---
+++ lib/next_gen/resources/repositories/file_storage_repository.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'fileutils'
+
+module NextGen
+  module Repositories
+    class FileStorageRepository
+      attr_reader :base_directory, :file_name
+
+      def initialize(base_directory, file_name)
+        @base_directory = File.join(Dir.pwd, base_directory)
+        @file_name = File.join(@base_directory, file_name)
+        @file_name.sub!('data', 'spec/data') if defined?(RSpec)
+      end
+
+      def save(data)
+        FileUtils.mkdir_p(File.dirname(file_name))
+        File.write(file_name, to_json(data))
+      end
+
+      def load
+        return JSON.parse(File.read(file_name)) if File.exist?(file_name)
+        nil
+      end
+
+      private
+
+      def to_json(data)
+        JSON.pretty_generate(data)
+      end
+    end
+  end
+end
