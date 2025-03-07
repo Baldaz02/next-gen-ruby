5d824950e194f03cf57644e6cf5890d8932299a0
Stefano Baldazzi <stefano.baldazzi@zero.it>
2025-03-07 14:17:35 +0100

    R360-04 refactor code

--- lib/next_gen/resources/services/crypto_report_service.rb
+++ lib/next_gen/resources/services/crypto_report_service.rb
@@ -14,32 +14,16 @@ module NextGen
       end
 
       def call
+        base_path = "data/#{Time.now.strftime('%Y-%m-%d %H')}"
+
         cryptos.each do |crypto|
           tickers = crypto.tickers
           indicators = Services::IndicatorService.new(tickers).calculate_all
 
-          file_path = data_file_path(crypto)
           data = Models::CryptoMarketData.new(crypto, tickers, indicators).to_h
-
-          save_data(file_path, data)
+          file_repo = Repositories::FileStorageRepository.new(base_path, "#{crypto.name}.json")
+          file_repo.save(data)
         end
-      end
-
-      private
-
-      def data_file_path(crypto)
-        base_path = File.join(Dir.pwd, 'data', Time.now.strftime('%Y-%m-%d %H'))
-        file_path = File.join(base_path, "#{crypto.name}.json")
-
-        file_path.sub!('data', 'spec/data') if defined?(RSpec)
-
-        FileUtils.mkdir_p(File.dirname(file_path))
-
-        file_path
-      end
-
-      def save_data(file_path, data)
-        File.write(file_path, JSON.pretty_generate(data))
       end
     end
   end
