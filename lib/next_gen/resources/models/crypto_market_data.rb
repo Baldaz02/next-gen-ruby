d76d9940478496d22dbee8d263ed9d926ae70f16
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-13 18:10:32 +0100

    R360-04 init to refactor

--- lib/next_gen/resources/models/crypto_market_data.rb
+++ lib/next_gen/resources/models/crypto_market_data.rb
@@ -3,82 +3,63 @@
 module NextGen
   module Models
     class CryptoMarketData
-      attr_reader :crypto, :tickers, :indicators
+      attr_reader :crypto, :tickers, :indicator_obj
 
       def initialize(crypto, tickers, indicators)
         @crypto = crypto
         @tickers = tickers
-        @indicators = indicators
+        @indicator_obj = Indicator.new(indicators: indicators)
       end
 
       def to_h
         crypto.to_h.merge({
-          data: Models::Ticker.sorted_by_date(tickers).map do |ticker|
-            ticker.to_h.merge(format_indicators(ticker.date))
-          end
-        })
+                            data: Models::Ticker.sorted_by_date(tickers).map do |ticker|
+                              ticker.to_h.merge(format_indicators(ticker.date))
+                            end
+                          })
       end
 
       private
 
-      def format_indicators(ticker_datetime)
-        {
-          sma_values: filter_by_datetime(indicators.sma_values, ticker_datetime),
-          ema_values: filter_by_datetime(indicators.ema_values, ticker_datetime),
-          macd_values: filter_by_datetime(indicators.macd_values, ticker_datetime),
-          rsi_values: filter_by_datetime(indicators.rsi_values, ticker_datetime),
-          sr_values: filter_by_datetime(indicators.sr_values, ticker_datetime),
-          tsi_values: filter_by_datetime(indicators.tsi_values, ticker_datetime),
-          bb_values: filter_by_datetime(indicators.bb_values, ticker_datetime),
-          atr_values: filter_by_datetime(indicators.atr_values, ticker_datetime),
-          kc_values: filter_by_datetime(indicators.kc_values, ticker_datetime),
-          obv_values: filter_by_datetime(indicators.obv_values, ticker_datetime),
-          fgi_values: filter_by_today(indicators.fgi_values, ticker_datetime)
-        }
-      end
-
-      def filter_by_datetime(values, datetime, index = 0)
-        unless values.any?(&:date_time)
-          all_values = []
-
-          [values.sma10, values.sma20, values.ema10, values.ema20].each_with_index do |indicator_list, index|
-            if indicator_list&.any?
-              all_values << filter_by_datetime(indicator_list, datetime, index)
-            end
-          end
-
-          return all_values.flatten
-        end
-
-        filtered_indicators = values.select { |value| value.date_time == datetime }
-        return [] if filtered_indicators.nil? || filtered_indicators.empty?
-
+      def datetime_builder(values)
         data = {}
-        filtered_indicators.each do |indicator|
-          indicator.instance_variables.each do |var|
-            attribute = "#{var}".to_s.delete('@')
+
+        values.each_slice(2) do |indicator, index|
+          indicator.instance_variables.map(&:to_s).each do |variable|
+            attribute = variable.delete('@')
             next if attribute == 'date_time'
 
-            if attribute == 'sma' || attribute == 'ema'
-              suffix = (index % 2 == 0) ? '10' : '20'
+            if %w[sma ema].include?(attribute)
+              suffix = index.even? ? '10' : '20'
               attribute = "#{attribute}#{suffix}"
             end
 
-            data[attribute.to_sym] = indicator.instance_variable_get(var)
+            data[attribute.to_sym] = indicator.instance_variable_get(variable)
           end
         end
         data
       end
 
-      def filter_by_today(data, ticker_datetime)
-        ticker_start_of_day = DateTime.new(ticker_datetime.year, ticker_datetime.month, ticker_datetime.day)
-        ticker_timestamp = ticker_start_of_day.to_time.to_i
+      def format_indicators(ticker_datetime)
+        %i[sma_values ema_values macd_values rsi_values sr_values tsi_values bb_values atr_values kc_values obv_values
+           fgi_values].each_with_object({}) do |type, hash|
+          method = type == :fgi_values ? :filter_by_today : :filter_by_datetime
+          indicators = indicator_obj.indicators
+          filtered_indicators = indicator_obj.send(method, indicators.send(type), ticker_datetime)
 
-        filtered_data = data.select do |d|
-          d.timestamp == ticker_timestamp
+          values = type == :fgi_values ? today_builder(filtered_indicators) : datetime_builder(filtered_indicators)
+          hash[type] = format_to_hash(values)
         end
+      end
 
-        filtered_data.map do |d|
+      def format_to_hash(values)
+        return values if values.is_a?(Hash)
+
+        values.reduce({}, :merge)
+      end
+
+      def today_builder(values)
+        values.map do |d|
           {
             value: d.value,
             value_classification: d.value_classification,
@@ -86,7 +67,7 @@ module NextGen
             time_until_update: d.time_until_update
           }
         end
-      end           
+      end
     end
   end
 end
