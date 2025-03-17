d0b8df00e88b889707948da89256d0b413f79e54
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-03-17 18:03:49 +0100

    R360-04 change organization of log files

--- lib/next_gen/config/logger.rb
+++ lib/next_gen/config/logger.rb
@@ -8,11 +8,12 @@ module NextGen
   module Config
     class Logger
       include Singleton
+      include NextGen::Helpers::FileHelper
 
       def initialize
-        log_directory = defined?(RSpec) ? 'spec/data/log' : 'data/log'
-        log_file = "#{log_directory}/#{Date.today.strftime('%Y-%m-%d')}.log"
-        FileUtils.mkdir_p(log_directory)
+        log_directory = defined?(RSpec) ? 'spec/' : File.expand_path('', Dir.pwd)
+        log_file = "#{log_directory}/#{base_path_day}/production.log"
+        FileUtils.mkdir_p(File.dirname(log_file))
 
         @logger = ::Logger.new(log_file, 'daily')
         @logger.level = ::Logger::DEBUG
