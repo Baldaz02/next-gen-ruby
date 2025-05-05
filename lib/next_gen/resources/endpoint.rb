d4669fe3444eedbefe89c47ee3aa7dd6cb477fcb
Baldaz02 <stefanobaldazzi40@gmail.com>
2025-05-05 18:49:58 +0200

    R360-07 add endpoint & relative controller

---
+++ lib/next_gen/resources/endpoint.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module NextGen
+  class Endpoint
+    attr_reader :logger, :params
+
+    def initialize(params)
+      ENV.fetch('APP_ENV', 'development')
+      Config::Application.set_timezone('GMT')
+      Config::SentryClient.setup
+
+      @params = params
+      set_datetime
+      @logger = Config::Logger.instance
+    end
+
+    def call
+      data = params[:data]
+      controller = Object.const_get(params[:controller_name]).new(data)
+      controller.send(params[:action_name])
+
+    rescue StandardError => e
+      logger.error("Error occured: #{e.message}")
+      Sentry.capture_exception(e)
+      raise e
+    end
+
+    private
+
+    def set_datetime
+      if params&.dig(:data, :timestamps, :end)
+        timestamp = params.dig(:data, :timestamps, :end) / 1000
+        datetime = Config::Application.timestamp_to_date(timestamp)
+        ENV['DATETIME'] = datetime.to_s
+      else
+        ENV['DATETIME'] = Time.now.to_s
+      end
+    end
+  end
+end
---
+++ spec/next_gen/resources/controllers/market_automation_controller_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.describe NextGen::Controllers::MarketAutomationController do
+  let(:data) { { interval: '1h', limit: 50 } }
+  let(:controller) { described_class.new(data) }
+
+  describe '#execute_job' do
+    it do
+      job_instance = instance_double(NextGen::Jobs::MarketAutomationJob)
+
+      expect(NextGen::Jobs::MarketAutomationJob).to receive(:new)
+        .with(data)
+        .and_return(job_instance)
+
+      expect(job_instance).to receive(:perform).and_return(true)
+
+      result = controller.execute_job
+      expect(result).to eq(true)
+    end
+  end
+end
